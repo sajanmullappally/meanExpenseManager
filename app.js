d999b4da988c8ba6bdad8205964ac4e7b3e88823
Sajan Mullappally <mullappallysajan@gmail.com>
2015-12-13 14:17:13 +0530

    add db connection

--- app.js
+++ app.js
@@ -4,17 +4,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
 // database connection =======================================================
 
-mongoose.connect('mongodb://localhost/meanExpenseManager');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log("successful database connection");
-});
+var db = require('./config/db');
 
 // parse application/json ====================================================
 
